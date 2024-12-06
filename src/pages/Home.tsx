f657286f0996b100001ac634b84559a889b37824
ybmin1 <yebin.min1@gmail.com>
2024-12-07 00:57:37 +0900

    Add BackgroundAnimationEffect component with PixiJS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import BackgroundAnimationEffect from "../components/BackgroundAnimationEffect";
+
 function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       Home
+      <BackgroundAnimationEffect />
     </div>
   );
 }
