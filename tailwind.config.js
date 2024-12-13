f0c165ac4cc296f8690f6b6213d877cc5ebd643b
ybmin1 <yebin.min1@gmail.com>
2024-12-14 01:58:17 +0900

    Install PixiJS and update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "cubic-bezier": "cubic-bezier(0.19, 1, 0.22, 1)",
+      },
+      transitionDuration: {
+        550: "550ms",
+      },
+    },
   },
   plugins: [],
 };
