9f8d94af96d6082ede5be4eb77738c84ac3993ca
ybmin1 <yebin.min1@gmail.com>
2024-12-20 00:31:40 +0900

    Add responsive design for Home page and update style for Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,23 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="w-screen h-[60px] fixed top-0 left-0 z-10 border-b border-gray-300">
-      <nav className="flex items-center justify-start h-full px-8 space-x-4">
-        <Link to="/">Logo</Link>
-        <Link to="/about">About</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/hobby">Hobby</Link>
-        <Link to="/contact">Contact</Link>
+    <div className="w-screen h-[60px] fixed top-0 left-0 z-10 border-b border-gray-300 text-white">
+      <nav className="flex items-center justify-start h-full px-8 space-x-6 text-xl">
+        <Link className="visited:text-current" to="/">
+          Logo
+        </Link>
+        <Link className="visited:text-current" to="/about">
+          About
+        </Link>
+        <Link className="visited:text-current" to="/portfolio">
+          Portfolio
+        </Link>
+        <Link className="visited:text-current" to="/hobby">
+          Hobby
+        </Link>
+        <Link className="visited:text-current" to="/contact">
+          Contact
+        </Link>
       </nav>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,15 @@ import BackgroundAnimationEffect from "../components/BackgroundAnimationEffect";
 
 const textContent = {
   hello: "Hello,",
-  iAm: "I'm",
+  iAm: "I'm ",
   button: "Work with me",
   myApproach: "My approach",
   userCenteredWeb: "User-Centred Web",
   myApproachDesc: `My approach is to create a web experience where users want to stay. I believe a good website makes users feel comfortable and at ease, while also offering fun aspects to capture their attention. To achieve this, I focus on building efficient and trendy websites by deepening my understanding of programming languages, writing clean code, and staying updated with new technologies.`,
 };
 const words = [
-  { text: " Yebin.", color: "text-green-500" },
-  { text: " a Web Developer.", color: "text-purple-500" },
+  { text: "Yebin.", color: "text-green-500" },
+  { text: "a Web Developer.", color: "text-purple-500" },
 ];
 
 function Home() {
@@ -103,38 +103,38 @@ function Home() {
     <div className="w-screen min-h-screen relative">
       <BackgroundAnimationEffect />
       <div className="absolute inset-0 mt-[60px] text-white">
-        <div className="h-[600px] flex flex-col justify-center items-start p-[100px]">
-          <h1 className="text-7xl font-bold pb-[10px]">{textContent.hello}</h1>
-          <div className="inline-flex">
-            <h1 className="relative inline-block text-7xl font-bold pb-[15px]">
-              <span>{textContent.iAm}</span>
-              <span className="relative">
-                {words.map((word, index) => (
-                  <span
-                    key={index}
-                    className={`absolute left-0 transition-opacity duration-[400ms] ${
-                      currentWordIndex === index ? "opacity-100" : "opacity-0"
-                    }`}
-                  >
-                    <span className={`inline-flex ${word.color}`}>
-                      {renderLetters(word.text, animationState)}
-                    </span>
+        <div className="h-[600px] flex flex-col justify-center items-start p-[10px] sm:p-[40px] md:p-[55px] lg:p-[100px] transition-all duration-300 ease-in-out">
+          <h1 className="font-bold pb-[10px] text-5xl sm:text-6xl md:text-7xl transition-all duration-300 ease-in-out">
+            {textContent.hello}
+          </h1>
+          <h1 className="relative inline-block font-bold whitespace-pre text-5xl sm:text-6xl md:text-7xl transition-all duration-300 ease-in-out">
+            <span className="pb-[10px]">{textContent.iAm}</span>
+            <div className="relative block md:inline transition-all duration-300 ease-in-out">
+              {words.map((word, index) => (
+                <span
+                  key={index}
+                  className={`absolute left-0 transition-opacity duration-[400ms] ${
+                    currentWordIndex === index ? "opacity-100" : "opacity-0"
+                  }`}
+                >
+                  <span className={`inline-flex ${word.color}`}>
+                    {renderLetters(word.text, animationState)}
                   </span>
-                ))}
-              </span>
-            </h1>
-          </div>
+                </span>
+              ))}
+            </div>
+          </h1>
           <button
-            className="my-[30px] inline-block bg-black border-2 border-white rounded-[30px] shadow-[4px_4px_0_0_white] text-white font-semibold text-[18px] px-[20px] text-center select-none touch-manipulation focus:outline-none focus-visible:outline-none active:shadow-[2px_2px_0_0_white] active:transform active:translate-x-[2px] active:translate-y-[2px] relative overflow-hidden group
+            className="inline-block px-[20px] bg-black border-2 border-white rounded-[30px] shadow-[4px_4px_0_0_white] text-white font-semibold text-center select-none touch-manipulation focus:outline-none focus-visible:outline-none active:shadow-[2px_2px_0_0_white] active:transform active:translate-x-[2px] active:translate-y-[2px] relative overflow-hidden group my-[80px] md:my-[30px] text-[15px] md:text-[18px] transition-all duration-300 ease-in-out
           "
           >
             <span className="relative z-20">{textContent.button}</span>
             <span className="absolute -left-[110px] -top-[50px] w-[50px] h-[200px] bg-white opacity-30 rotate-[35deg] -z-100 transition-all duration-550 ease-cubic-bezier group-hover:left-full"></span>
           </button>
         </div>
-        <div className="h-[600px] flex justify-end items-start p-[100px]">
-          <div className="w-1/2">
-            <h2 className="mb-[50px] text-5xl font-bold">
+        <div className="h-[600px] flex items-start justify-start sm:justify-end p-[25px] sm:p-[40px] md:p-[55px] lg:p-[100px] transition-all duration-300 ease-in-out">
+          <div className="w-full min-w-[350px] sm:w-2/3 md:w-1/2 transition-all duration-300 ease-in-out">
+            <h2 className="mb-[50px] font-bold text-4xl md:text-5xl ransition-all duration-300 ease-in-out">
               {textContent.myApproach}
             </h2>
             <h3 className="mb-[10px] text-2xl font-bold">
