cad95dbc8b20cb850adc13162b138c47b9985a54
ybmin1 <yebin.min1@gmail.com>
2025-03-31 19:29:02 +0900

    Install Jest and update configuration

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  },
+};
