7ce8089f7ab9790a0aedc8fddb4d5bf9d9f0e153
ybmin1 <yebin.min1@gmail.com>
2025-04-02 18:45:25 +0900

    Update configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,11 @@ module.exports = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(png|jpg|jpeg|gif|svg)$": "jest-transform-stub", // 이미지 파일 변환 설정 추가
   },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|svg)$": "jest-transform-stub", // 이미지 파일 매핑 설정 추가
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironmentOptions: {
